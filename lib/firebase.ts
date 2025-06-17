f74354f1ebece89beb925b69f7d31bacc5994f58
Ryuji0721 <24aw0115@jec.ac.jp>
2025-06-17 10:32:11 +0900

    fix:エラー修正

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// lib/firebase.ts
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzaJ2ARWuKZYLBxN_50eHrWcqHtv3yolc",
-  authDomain: "spoddymyapp.firebaseapp.com",
-  projectId: "spoddymyapp",
-  storageBucket: "spoddymyapp.firebasestorage.app",
-  messagingSenderId: "590633078134",
-  appId: "1:590633078134:web:55c119ba91935d64253e57",
-  measurementId: "G-SPBQE6L7ZK"
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 export const db = getFirestore(app);
